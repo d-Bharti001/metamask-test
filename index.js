ba0864f0d7eecf260714187092ca5a763a548edd
dbharti <swansi.dharmveer@gmail.com>
2021-04-09 23:37:05 +0530

    Add css styles and update index.js

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
-let web3;
-let accounts;
-
 async function initialize() {
-    const provider = await detectEthereumProvider();
-    if (provider && provider.isMetaMask) {
-        if (provider.chainId == "0x5") {
-            startApp(provider);
+
+    if (ethereum && ethereum.isMetaMask) {
+
+        let chainId = await ethereum.request({method: "eth_chainId"});
+
+        if (chainId == "0x5") {
+            startApp(ethereum);
         }
         else {
             alert("Please switch to Goerli Test network\n and make sure you have an account in it.");
         }
     }
+
     else {
         alert("Please install Metamask extension and create an account on Goerli test network.\
         \nNo functionality can work without it.");
     }
 }
 
-async function startApp(provider) {
+async function startApp(ethereum) {
 
-    web3 = new Web3(provider);
-
-    accounts = await provider.request({method: 'eth_requestAccounts'});
-
-    let info = document.getElementById("info");
-    info.innerHTML = accounts[0];
+    let web3 = new Web3(ethereum);
 }
 
 window.addEventListener("DOMContentLoaded", initialize);
