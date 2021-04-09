927dca156f779d23e4d9586d6dbe689a14323d45
dbharti <swansi.dharmveer@gmail.com>
2021-04-09 12:00:53 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+var web3;
+
+async function initialize() {
+
+    const provider = await detectEthereumProvider();
+
+    if (provider) {
+        web3 = new Web3(provider);
+        console.log("web3 loaded");
+        var accounts = await provider.request({method: 'eth_requestAccounts'});
+    }
+    else {
+        console.log("Metamask not detected");
+    }
+    
+}
+
+
+window.addEventListener("DOMContentLoaded", initialize);
