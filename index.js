0bfdf5614cfa1fd62a2f5789d3100906647c672f
dbharti <swansi.dharmveer@gmail.com>
2021-04-09 12:22:51 +0530

    fix some bugs

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-var web3;
+async function initialize() {
+    const provider = await detectEthereumProvider();
+    if (provider) {
+        load_web3(provider);
+    }
+    else {
+        console.log("Metamask not detected");
+    } 
+}
 
-async function load_web3() {
-    web3 = await new Web3(provider);
+async function load_web3(provider) {
+    const web3 = await new Web3(provider);
     console.log("web3 loaded");
     var accounts = await provider.request({method: 'eth_requestAccounts'});
 
     let info = document.getElementById("info");
     info.innerHTML = accounts[0];
-}
-
-async function initialize() {
-
-    const provider = await detectEthereumProvider();
-
-    if (provider) {
-        load_web3();
-    }
-    else {
-        console.log("Metamask not detected");
-    }
-    
 }
 
 window.addEventListener("DOMContentLoaded", initialize);
