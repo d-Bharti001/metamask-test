f333144c499bd5c3283f24c1a9b20f7df73fcfe1
dbharti <swansi.dharmveer@gmail.com>
2021-04-09 12:18:54 +0530

    Add new function

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-
 var web3;
+
+async function load_web3() {
+    web3 = await new Web3(provider);
+    console.log("web3 loaded");
+    var accounts = await provider.request({method: 'eth_requestAccounts'});
+
+    let info = document.getElementById("info");
+    info.innerHTML = accounts[0];
+}
 
 async function initialize() {
 
     const provider = await detectEthereumProvider();
 
     if (provider) {
-        // web3 = new Web3(provider);
-        console.log("web3 loaded");
-        //var accounts = await provider.request({method: 'eth_requestAccounts'});
+        load_web3();
     }
     else {
         console.log("Metamask not detected");
     }
     
 }
-
 
 window.addEventListener("DOMContentLoaded", initialize);
