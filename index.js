68a1385f7d112777f134445b010faebb23d38141
dbharti <swansi.dharmveer@gmail.com>
2021-04-09 19:50:24 +0530

    Add initial frontend-only interface

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
+let web3;
+let accounts;
+
 async function initialize() {
     const provider = await detectEthereumProvider();
-    if (provider) {
-        load_web3(provider);
+    if (provider && provider.isMetaMask) {
+        if (provider.chainId == "0x5") {
+            startApp(provider);
+        }
+        else {
+            alert("Please switch to Goerli Test network\n and make sure you have an account in it.");
+        }
     }
     else {
-        console.log("Metamask not detected");
-    } 
+        alert("Please install Metamask extension and create an account on Goerli test network.\
+        \nNo functionality can work without it.");
+    }
 }
 
-async function load_web3(provider) {
-    const web3 = await new Web3(provider);
-    console.log("web3 loaded");
-    var accounts = await provider.request({method: 'eth_requestAccounts'});
+async function startApp(provider) {
+
+    web3 = new Web3(provider);
+
+    accounts = await provider.request({method: 'eth_requestAccounts'});
 
     let info = document.getElementById("info");
     info.innerHTML = accounts[0];
