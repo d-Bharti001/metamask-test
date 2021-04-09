d95fd5f3c6c3dc7b19bdab33189313ce49217606
dbharti <swansi.dharmveer@gmail.com>
2021-04-09 12:13:49 +0530

    remove new Web3(provider)

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ async function initialize() {
     const provider = await detectEthereumProvider();
 
     if (provider) {
-        web3 = new Web3(provider);
+        // web3 = new Web3(provider);
         console.log("web3 loaded");
         //var accounts = await provider.request({method: 'eth_requestAccounts'});
     }
